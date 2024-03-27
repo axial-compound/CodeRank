402ca2c077917c4ebdcc6b507689c20c7a0cece4
Ayush Jha <ashujha301@gmail.com>
2024-03-27 18:02:19 +0530

    add middleware

---
+++ server/APIs/middleware/authMiddleware.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const jwt = require('json-web-token');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+
+const authMiddleware = async(req,res,next) =>{
+
+}
