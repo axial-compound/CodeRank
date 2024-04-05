fb2cb0c509f31184f9304edd798211db2e80a61b
Ayush Jha <ashujha301@gmail.com>
2024-04-06 02:22:10 +0530

    Add:Dockerfile/container/images

--- server/APIs/routes/unAuthRoutes.js
+++ server/APIs/routes/unAuthRoutes.js
@@ -4,6 +4,9 @@ const authMiddlewarefunc  = require('../../middleware/authMiddleware');
 
 const unAuthControllers = require('../controllers/unAuthControllers');
 
+router.get('/hey', (req,res)=>{
+    res.json({msg:'Hey there!'});
+});
 router.post('/login',unAuthControllers.login);
 router.post('/register', unAuthControllers.register);
 
