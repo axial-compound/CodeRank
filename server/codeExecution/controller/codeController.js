f6acf5cba4f7921f8223a537a3f86d680d0346d6
Ayush Jha <ashujha301@gmail.com>
2024-04-11 14:45:47 +0530

    Update:Docker image build failed

--- server/codeExecution/controller/codeController.js
+++ server/codeExecution/controller/codeController.js
@@ -1,22 +1,78 @@
-const codeService = require('../services/codeServices');
+const stream = require('stream');
+const Dockerode = require('dockerode');
+const docker = new Dockerode();
 
-const getCode = async(req,res) => {
-
+const runCode = async (req, res) => {
   try {
     const language = req.params.language;
     const codeBody = req.body;
 
-    if(!codeBody || !language){
-      res.status(404).json({message: "Language or Code Body is missing"});
+    if (!codeBody ||!language) {
+      res.status(404).json({ message: "Language or Code Body is missing" });
     }
 
-    const user = await codeService.runCodeBody(codeBody,language);
+    let output = "";
+    let error = "";
 
+    const dockerImages = {
+      python: "python-compiler",
+      cpp: "cpp-compiler",
+      java: "java-env",
+    };
 
-    return user;
+    const dockerCommands = {
+      python: ["python", "-c"],
+      cpp: ["sh", "-c", "g++ -o my_cpp_program -xc++ - &&./my_cpp_program"],
+      java: [
+        "sh",
+        "-c",
+        'echo "$1" > Main.java && javac Main.java && java Main',
+      ],
+    };
+
+    const image = dockerImages[language];
+    const command = dockerCommands[language];
+
+    const container = await docker.run(
+      image,
+      command,
+      { codeBody },
+      (process.stdin, process.stdout, process.stderr)
+    );
+
+    await new Promise((resolve, reject) => {
+      container.wait((err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+
+    const inspectData = await container.inspect();
+
+    output = inspectData.State.Running
+     ? "Code executed successfully"
+      : "Code execution failed";
+
+    if (inspectData.State.ExitCode!== 0) {
+      error = `Process exited with code ${inspectData.State.ExitCode}`;
+    }
+
+    const timestamp = new Date().toISOString();
+
+    const response_data = {
+      output: output,
+      error: error,
+      execution_info: `Code executed at ${timestamp}`,
+    };
+
+    return response_data;
   } catch (error) {
-      res.status(500).json({msg:"Error Occured: ", error: error});
+    console.log("Error executing code", error);
+    throw error;
   }
 };
 
-module.exports = {getCode}
+module.exports = { runCode };
--- server/codeExecution/routes/routes.js
+++ server/codeExecution/routes/routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const codeController = require('../controller/codeController');
 
-router.post('/:language',codeController.getCode);
+router.post('/:language',codeController.runCode);
 
 module.exports = router;
