d7adc5c5f6a0eb7089492935beab9c54b7119dcc
Ayush Jha <ashujha301@gmail.com>
2024-04-13 15:20:10 +0530

    feat:home page animation

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,20 +1,36 @@
-import { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import './home.css';
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./home.css";
+
+const sentences = [
+  "Cloud-based code editor",
+  "Seamless coding experience",
+  "Coding Redefined",
+  "Transforming the way you code",
+];
 
 const Home = () => {
-  const [animate, setAnimate] = useState(false);
+  const [currentSentence, setCurrentSentence] = useState(0);
 
   useEffect(() => {
-    setAnimate(true);
+    const interval = setInterval(() => {
+      setCurrentSentence((prev) => (prev + 1) % sentences.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className={`home ${animate ? 'animate' : ''}`}>
-      <h1>Welcome to <span className="code-animation">CodeRank</span></h1>
-      <h4><span className="editor-animation">Cloud Code Editor</span></h4>
+    <div className="home">
+      <h1>Welcome to CodeRank &lt;/&gt;</h1>
+      <h4>
+        <span className="editor-animation">{sentences[currentSentence]}</span>
+      </h4>
       <NavLink to="/login">
-        <button className="glow-on-hover" type="button">Login To Continue</button>
+        <button className="glow-on-hover" type="button">
+          Login To Continue &nbsp;
+          <i className="fa fa-arrow-circle-right" aria-hidden="true"></i>
+        </button>
       </NavLink>
       <div>
         <NavLink to="/editor">
